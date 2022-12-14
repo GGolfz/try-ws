343319f2e8d383c5fa4058bfb0ab3d983e98df1e
GGolfz <wisarutgolf11@gmail.com>
2022-12-14 21:15:20 +0700

    add: simple send message to room :sparkles:

--- client.ts
+++ client.ts
@@ -1,10 +1,20 @@
-import WebSocket from 'ws';
+import WebSocket from "ws";
 
-const ws = new WebSocket('ws://localhost:5050');
+const ws = new WebSocket("ws://localhost:5050");
 
-// Simulate Message Sending
+ws.on("open", () => {
+  console.log("connected to server");
+  ws.send(JSON.stringify({eventName: 'joinRoom', payload: {room: 'room1'}}));
+  setTimeout(() => {
+    ws.send(JSON.stringify({eventName: 'sendMessage', payload: {room: 'room1', message: 'hello'}}))
+  }, 3000)
+});
 
-ws.on('open', () => {
-    console.log('connected to server');
-    ws.send('hello from client');
+ws.on("message", (data) => {
+    console.log("received: %s", data);
 })
+
+ws.on("close", () => {
+  console.log("disconnected");
+});
+
--- index.ts
+++ index.ts
@@ -4,9 +4,31 @@ const wss = new WebSocketServer({
     port: 5050,
 });
 
+const Room: {[id: string]: WebSocket[]} = {}
+
 wss.on('connection', (ws: WebSocket) => {
     ws.on('message', (message: string) => {
         console.log('received: %s', message);
+        const {eventName, payload} = JSON.parse(message)
+        ws.emit(eventName, payload)
+    })
+    ws.on('joinRoom', (data: any) => {
+        const {room} = data
+        if (!Room[room]) {
+            Room[room] = []
+        }
+        Room[room].push(ws)
+    })
+    ws.on('leaveRoom', (data: any) => {
+        const {room} = data
+        Room[room] = Room[room].filter((socket: WebSocket) => socket !== ws)
+    })
+    ws.on('sendMessage', (data: any) => {
+        const {room, message} = data
+        Room[room].forEach((socket: WebSocket) => {
+            if(socket === ws) return
+            socket.send(JSON.stringify({eventName: 'message', payload: {message}}))
+        })
     })
 });
 wss.on('listening', () => {
