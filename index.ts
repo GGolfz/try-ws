2e11394509dc4558913860680f4cd3e8b95806a1
GGolfz <wisarutgolf11@gmail.com>
2022-12-21 20:23:17 +0700

    update: room functionality

--- index.ts
+++ index.ts
@@ -1,36 +1,48 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({
-    port: 5050,
+  port: 5050,
 });
 
-const Room: {[id: string]: WebSocket[]} = {}
+const Room: { [id: string]: WebSocket[] } = {};
 
-wss.on('connection', (ws: WebSocket) => {
-    ws.on('message', (message: string) => {
-        console.log('received: %s', message);
-        const {eventName, payload} = JSON.parse(message)
-        ws.emit(eventName, payload)
-    })
-    ws.on('joinRoom', (data: any) => {
-        const {room} = data
-        if (!Room[room]) {
-            Room[room] = []
-        }
-        Room[room].push(ws)
-    })
-    ws.on('leaveRoom', (data: any) => {
-        const {room} = data
-        Room[room] = Room[room].filter((socket: WebSocket) => socket !== ws)
-    })
-    ws.on('sendMessage', (data: any) => {
-        const {room, message} = data
-        Room[room].forEach((socket: WebSocket) => {
-            if(socket === ws) return
-            socket.send(JSON.stringify({eventName: 'message', payload: {message}}))
-        })
-    })
+const emit = (eventName: string, payload: any) => {
+  return JSON.stringify({ eventName, payload });
+};
+wss.on("connection", (ws: WebSocket) => {
+  ws.on("message", (message: string) => {
+    const { eventName, payload } = JSON.parse(message);
+    if (!eventName || !payload) return;
+    ws.emit(eventName, payload);
+  });
+  ws.on("joinRoom", (data: any) => {
+    const { room } = data;
+    if (!Room[room]) {
+      Room[room] = [];
+    }
+    Room[room].push(ws);
+  });
+  ws.on("leaveRoom", (data: any) => {
+    const { room } = data;
+    Room[room] = Room[room].filter((socket: WebSocket) => socket !== ws);
+  });
+  ws.on("sendMessage", (data: any) => {
+    const { room, message } = data;
+    Room[room].forEach((socket: WebSocket) => {
+      if (socket === ws) return;
+      socket.send(emit("message", { message }));
+    });
+  });
+  ws.on("close", () => {
+    Object.keys(Room).forEach((room: string) => {
+      Room[room] = Room[room].filter((socket: WebSocket) => socket !== ws);
+    });
+  });
+  ws.on("getRooms", () => {
+    const rooms = Object.keys(Room).map((room) => ({ name: room, member: Room[room] }));
+    ws.send(emit("rooms", { rooms: rooms }));
+  });
 });
-wss.on('listening', () => {
-    console.log('listening on port 5050');
-})
+wss.on("listening", () => {
+  console.log("listening on port 5050");
+});
